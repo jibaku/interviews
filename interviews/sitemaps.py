8e6af2ed98b20667d613ae0d74881bfa4cb6172d
Fabien Schwob <github@x-phuture.com>
2012-12-27 01:46:14 +0100

    * Added site maps

---
+++ interviews/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from interviews.models import Interview
+
+class InterviewSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return Interview.objects.published()
+
+    def lastmod(self, obj):
+        return obj.updated_on
